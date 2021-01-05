ac54295b9d92f880e66dcb633e7e2d723177f690
MrR3set <bairon.pazrobles@gmail.com>
2021-01-04 20:29:17 -0800

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.scss';
-import Header from './components/Header'
+import { Route, Switch } from 'react-router-dom'
+import HomePage from './Pages/HomePage/HomePage'
+
+import Header from './components/Header';
+
+
 
 function App() {
   return (
     <div className="App">
       <Header className='nav'>muga.ly</Header>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path='*'> Page not found or not yet developed, come back later</Route>
+      </Switch>
+
+
+
     </div>
   );
 }
