d6b703c89e8b00b13cb94c6288982a4ed94c7cae
MrR3set <bairon.pazrobles@gmail.com>
2021-01-04 20:53:25 -0800

    Custom link component updated props

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CustomLink({title,path,size='',info}) {
+function CustomLink({title,path,size='',background='#AE5D68'}) {
     return( 
-        <Link to={path} className={`option wrapper link ${size}`}>
+        <Link to={path} className={`customlink ${size}`} style={{background:background}}>
             <h1 className="title">{title}</h1>
         </Link>
     );
