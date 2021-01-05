4112237c6386caa8a566dacfd879a4185e2953cd
MrR3set <bairon.pazrobles@gmail.com>
2021-01-04 20:53:34 -0800

    Updated properties for certain custom links

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,17 +4,17 @@ import CustomLink from '../../components/CustomLink'
 
 function HomePage() {
   return (
-    <div className="HomePage">
+    <div className="HomePage page">
  
         <div className='input'>
             <input type='text'></input>
         </div>
 
         <div className="links">
-            <CustomLink path='mylinks' title='mylinks'></CustomLink>
-            <CustomLink path='profile' title='profile'></CustomLink>
-            <CustomLink path='signout' title='sign out'></CustomLink>
-            <CustomLink path='moreinfo' title='more info'></CustomLink>
+            <CustomLink path='mylinks' title='My Links' background='#F54925'></CustomLink>
+            <CustomLink path='profile' title='Profile' size='tall'></CustomLink>
+            <CustomLink path='signout' title='Sign out' background='#660F56'></CustomLink>
+            <CustomLink path='moreinfo' title='More info' size='wide' background='#D1193E'></CustomLink>
         </div>
 
 
