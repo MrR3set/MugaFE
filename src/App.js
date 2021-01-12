7234691ef544f80b782e6ba046c8f60c9b9895a6
MrR3set <bairon.pazrobles@gmail.com>
2021-01-11 22:04:49 -0800

    Firebase init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import RedirectPage from './Pages/RedirectPage/RedirectPage';
 import Header from './components/Header';
 
 
+import firebase from 'firebase/app';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  // this key is safe to expose, you wont get nothing by copying them - 
+  // https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public + direct contact to firebase support
+    apiKey: "AIzaSyDfeI8Pp3PtOxSsl-8SD39whRli9RWcs-I",
+    authDomain: "mugaly-f1fc4.firebaseapp.com",
+    projectId: "mugaly-f1fc4",
+    storageBucket: "mugaly-f1fc4.appspot.com",
+    messagingSenderId: "653100497992",
+    appId: "1:653100497992:web:122b93720aede80b102c7d",
+    measurementId: "G-658V5ZSFG4" 
+})
+
+const firestore = firebase.firestore();
+
 
 function App() {
   return (
