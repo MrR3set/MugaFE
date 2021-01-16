cf7da1e4778fab91798ea22b6ea52234aa3b6b38
MrR3set <bairon.pazrobles@gmail.com>
2021-01-15 22:56:26 -0800

    Cleaned up

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './HomePage.scss';
 import CustomLink from '../../components/CustomLink';
 import firebase from 'firebase/app';
