eb8dc6305c3ba30217359fe47f1a08da1ce87606
MrR3set <bairon.pazrobles@gmail.com>
2021-02-01 20:11:13 -0800

    Changed order of links

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -62,8 +62,8 @@ function HomePage({linkRef}) {
         <div className="links">
             <CustomLink path='mylinks' title='My Links' background='#F54925'></CustomLink>
             <CustomLink path='profile' title='Profile' size='tall'></CustomLink>
-            <CustomLink path='moreinfo' title='More info' size='wide' background='#D1193E'></CustomLink>
-            <CustomLink path='signout' title='Sign out' background='#660F56'></CustomLink>
+            <CustomLink path='moreinfo' title='More info' background='#D1193E'></CustomLink>
+            <CustomLink path='signout' title='Sign out' size="wide" background='#660F56'></CustomLink>
         </div>
 
 
